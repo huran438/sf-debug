788a3554753ddc35eb022b2441265f7b8026dfec
Ilya Khurani <huran438@gmail.com>
2024-03-29 00:38:01 +0300

    init

---
+++ Runtime/SFDebug.cs
@@ -0,0 +1,12 @@
+﻿namespace SFramework.Debug.Runtime
+{
+    public static class SFDebug
+    {
+        public static bool IsDebug { get; private set; }
+
+        public static void SetDebug(bool isDebug)
+        {
+            IsDebug = isDebug;
+        }
+    }
+}
